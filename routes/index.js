cb261adde2e424c3da6ba3cbbff585c2df4fd177
Makoto Ishida <makotoishida@gmail.com>
2019-10-27 11:36:12 -1000

    Added mysql2 and sequelize, eslint.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   const dayHeaders = [
     { name: 'Sun', weekend: true },
     { name: 'Mon' },

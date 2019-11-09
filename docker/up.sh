cbf45565d622199dfd159c2e6524f9a81cd70f65
Makoto Ishida <makotoishida@gmail.com>
2019-11-08 15:41:59 -1000

    MySQLをDockerで動かしてNodeアプリはローカルで動かす場合の設定を追加。

---
+++ docker/up.sh
@@ -0,0 +1,9 @@
+# If you start up locally, you just need to start MySQL using Docker.
+docker-compose -f ../docker-compose.yml up -d mysql
+
+# Run the app by running:
+#   npm run dev
+#    or
+#   npm start
+#
+

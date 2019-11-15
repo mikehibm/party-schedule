ab21c5db1c5cb5ce6f3e283dcaf05d2e9465133d
Makoto Ishida <makotoishida@gmail.com>
2019-11-14 21:15:59 -1000

    Hide logout button after user logged out.

--- routes/auth-helper.js
+++ routes/auth-helper.js
@@ -1,10 +1,14 @@
 module.exports = (req, res, next) => {
   if (!req.session.user) {
+    // Remove user info from app.locals.
+    delete req.app.locals.user;
+
     res.redirect('/login');
     return;
   }
 
   // Make user info available in view files.
   req.app.locals.user = req.session.user;
+
   next();
 };

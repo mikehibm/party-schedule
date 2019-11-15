f0bd5a78a24159a0dc3ae922048537df05cfc2e3
Makoto Ishida <makotoishida@gmail.com>
2019-11-14 22:19:55 -1000

    Fix a bug in the auth-helper middleware. Use res.locals instead of req.app.locals.

--- routes/auth-helper.js
+++ routes/auth-helper.js
@@ -1,14 +1,14 @@
 module.exports = (req, res, next) => {
   if (!req.session.user) {
-    // Remove user info from app.locals.
-    delete req.app.locals.user;
+    // Remove user info from res.locals.
+    delete res.locals.user;
 
     res.redirect('/login');
     return;
   }
 
   // Make user info available in view files.
-  req.app.locals.user = req.session.user;
+  res.locals.user = req.session.user;
 
   next();
 };

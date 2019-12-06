73a1ab69b5e725da41cba5e3cf17ec0b6fc5d480
Makoto Ishida <makotoishida@gmail.com>
2019-12-05 17:51:51 -1000

    Implement user edit/delete, change password.

--- routes/users.js
+++ routes/users.js
@@ -3,42 +3,51 @@ const router = express.Router();
 const db = require('../db/models/');
 const authAdmin = require('./auth-admin-helper');
 
-/* List users */
+// List users
 router.get('/', authAdmin, async (req, res) => {
+  const message = req.session.message;
+  req.session.message = undefined;
+
   const users = await db.User.findAll();
-  res.render('users', { users });
+  res.render('users', { title: 'Users', users, message });
 });
 
-/* Edit user */
+// Show Edit user screen
 router.get('/edit/:id', authAdmin, async (req, res) => {
   const viewFile = 'users_edit';
+  const message = req.session.message;
+  req.session.message = undefined;
+
   const id = req.params.id | 0;
+  const isNew = id === 0;
   let user = db.User.build({ id: 0 });
 
-  if (id) {
+  if (!isNew) {
     user = await db.User.findByPk(id, { raw: true });
   }
 
   if (!user) {
-    res.render(viewFile, { user, error: 'User not found' });
+    res.render(viewFile, { title: 'User', user, error: 'User not found' });
     return;
   }
 
   delete user.password;
-  res.render(viewFile, { user, isNew: user.id === 0 });
+  res.render(viewFile, { title: 'User', user, isNew, message });
 });
 
+// Create/Update user
 router.post('/edit/:id', authAdmin, async (req, res) => {
   const viewFile = 'users_edit';
   const id = req.params.id | 0;
+  const isNew = id === 0;
   let user = db.User.build({ id: 0 });
 
-  if (id) {
+  if (!isNew) {
     user = await db.User.findByPk(id);
   }
 
   if (!user) {
-    res.render(viewFile, { user, error: 'User not found' });
+    res.render(viewFile, { title: 'User', user, error: 'User not found' });
     return;
   }
 
@@ -47,26 +56,23 @@ router.post('/edit/:id', authAdmin, async (req, res) => {
     user.email = req.body.email;
     user.isAdmin = req.body.isAdmin === '1';
 
-    if (user.id === 0) {
+    if (isNew) {
       user.password = req.body.password;
     }
 
-    const savedUser = await user.save();
-    savedUser.password = undefined;
+    await user.save();
 
     // eslint-disable-next-line require-atomic-updates
-    req.session.message = 'User was successfully saved.';
-    res.redirect(req.originalUrl);
+    req.session.message = 'User was saved successfully.';
+
+    res.redirect(`/users/edit/${user.id}`);
   } catch (err) {
     user.password = undefined;
-    res.render(viewFile, { user, error: err.message });
-    return;
+    res.render(viewFile, { title: 'User', user, isNew, error: err.message });
   }
-
-  return;
 });
 
-/* Edit user */
+// Show Change password screen
 router.get('/change_pwd/:id', authAdmin, async (req, res) => {
   const viewFile = 'users_change_pwd';
   const id = req.params.id | 0;
@@ -77,16 +83,22 @@ router.get('/change_pwd/:id', authAdmin, async (req, res) => {
   }
 
   if (!user) {
-    res.render(viewFile, { user, error: 'User not found' });
+    res.render(viewFile, {
+      title: 'User - Change Password',
+      user,
+      error: 'User not found',
+    });
     return;
   }
 
   delete user.password;
-  res.render(viewFile, { user });
+  res.render(viewFile, { title: 'User - Change Password', user });
 });
 
+// Change User password
 router.post('/change_pwd/:id', authAdmin, async (req, res) => {
   const viewFile = 'users_change_pwd';
+
   const id = req.params.id | 0;
   let user = db.User.build();
 
@@ -95,27 +107,55 @@ router.post('/change_pwd/:id', authAdmin, async (req, res) => {
   }
 
   if (!user) {
-    res.render(viewFile, { user, error: 'User not found' });
+    res.render(viewFile, {
+      title: 'User - Change Password',
+      user,
+      error: 'User not found',
+    });
     return;
   }
 
   try {
     // Automatically hashed by the 'Setter' function defined in the model.
     user.password = req.body.password;
-
     await user.save();
+
+    user.password = undefined;
+
+    // eslint-disable-next-line require-atomic-updates
+    req.session.message = `User's password was changed successfully.`;
+
+    res.redirect(`/users/edit/${user.id}`);
   } catch (err) {
     user.password = undefined;
-    res.render(viewFile, { user, error: err.message });
+    res.render(viewFile, {
+      title: 'User - Change Password',
+      user,
+      error: err.message,
+    });
+  }
+});
+
+// Delete User
+router.post('/delete/:id', authAdmin, async (req, res) => {
+  const id = req.params.id | 0;
+  let user = await db.User.findByPk(id);
+  if (!user) {
+    res.render('user_edit', { title: 'User', user, error: 'User not found' });
     return;
   }
 
-  user.password = undefined;
-  res.render(viewFile, {
-    user,
-    message: `User's password was successfully changed.`,
-  });
-  return;
+  try {
+    await user.destroy();
+
+    // eslint-disable-next-line require-atomic-updates
+    req.session.message = 'User was deleted successfully.';
+
+    res.redirect('/users/');
+  } catch (err) {
+    user.password = undefined;
+    res.render('user_edit', { title: 'User', user, error: err.message });
+  }
 });
 
 module.exports = router;
